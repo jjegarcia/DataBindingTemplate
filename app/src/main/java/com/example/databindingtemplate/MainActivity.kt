f7b881b5b08689788a32f812b1d8749fe16914e7
Jenaro Garcia <jgarc609@ford.com>
2019-10-22 20:34:45 +0100

    Refactor Binding to generic format. Deleted comments

--- app/src/main/java/com/example/databindingtemplate/MainActivity.kt
+++ app/src/main/java/com/example/databindingtemplate/MainActivity.kt
@@ -9,17 +9,8 @@ import com.example.databindingtemplate.databinding.ActivityMainBinding
 import com.example.databindingtemplate.VM as VM1
 
 class MainActivity : AppCompatActivity() {
-//    private val viewModel by lazy {
-//        ViewModelProviders.of(this).get(VM::class.java)
-//    }
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//       val binding: ActivityMainBinding = DataBindingUtil.setContentView(
-//            this, R.layout.activity_main
-//        )
-//        binding.viewModel = viewModel
         ActivityMainBinding.inflate(LayoutInflater.from(this)).apply {
             setContentView(root)
             viewModel =com.example.databindingtemplate.VM()
