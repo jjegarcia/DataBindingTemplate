c63033a7e91ce2cd02f5b15912de6e33743806ef
Jenaro Garcia <jgarc609@ford.com>
2019-10-22 17:43:18 +0100

    removed redundant code on VM

---
+++ app/src/main/java/com/example/databindingtemplate/TileViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.databindingtemplate
+
+import androidx.lifecycle.ViewModel
+
+class TileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+    val tileText="Tile"
+}
