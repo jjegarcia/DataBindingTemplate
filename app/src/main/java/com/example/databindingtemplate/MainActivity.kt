3ee84a14298926eccd14693a8d0c74c3cbe9723c
Jenaro Garcia <jgarc609@ford.com>
2019-11-14 13:46:58 +0000

    40 Cards fitting grid layout manager by fixing card tile to  6dp pad left and 64x64 dp cards

--- app/src/main/java/com/example/databindingtemplate/MainActivity.kt
+++ app/src/main/java/com/example/databindingtemplate/MainActivity.kt
@@ -6,6 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.databindingtemplate.databinding.ActivityMainBinding
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+//import sun.jvm.hotspot.utilities.IntArray
+
+
 
 class MainActivity : AppCompatActivity() {
     //lateinit var cardList :ArrayList<Card>
@@ -25,14 +32,54 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val card = Card("icon1", R.drawable.ic_launcher_foreground)
+        val card20=Card("icon20",R.drawable.avatar_1_raster)
+        val card2=Card("icon20",R.drawable.avatar_2_raster)
+        val card3=Card("icon20",R.drawable.avatar_3_raster)
+
 
         var cardList = ArrayList<Card>()
-        cardList.add(card)
-        cardList.add(card)
-        cardList.add(card)
-        cardList.add(card)
-        cardList.add(card)
-        cardList.add(card)
+        cardList.add(card20)//1
+        cardList.add(card20)//1
+        cardList.add(card20)//1
+        cardList.add(card20)//1
+        cardList.add(card20)//1
+        cardList.add(card20)//1
+        cardList.add(card20)//1
+        cardList.add(card20)//1
+        cardList.add(card20)//1
+        cardList.add(card20)//1
+        cardList.add(card20)//1
+        cardList.add(card20)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card2)//1
+        cardList.add(card3)//1
+        cardList.add(card3)//1
+        cardList.add(card3)//1
+        cardList.add(card3)//1
+        cardList.add(card3)//1
+        cardList.add(card3)//1
+        cardList.add(card3)//1
+        cardList.add(card3)//1
+        cardList.add(card3)//1
+
 
         val adapter = Adapter(cardList)
 
@@ -40,7 +87,12 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView.adapter=adapter
 
-     //   adapter.setData()
+       val manager = GridLayoutManager(this, 5, GridLayoutManager.VERTICAL, false)
+
+               recyclerView.layoutManager = manager
+
+
+        //   adapter.setData()
 
 
 
